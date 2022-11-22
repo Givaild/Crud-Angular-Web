c59b47ec31fe15d3a4b7fc27b63c2c2218d8573b
Givaild <gil_nm@hotmail.com>
2022-11-21 22:33:19 -0300

    feat: for create product

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,9 @@ import { ProductCreateComponent } from './components/product/product-create/prod
 import {MatButtonModule} from '@angular/material/button';
 import { MatSnackBarModule} from '@angular/material/snack-bar';
 import {HttpClientModule} from '@angular/common/http'
+import { FormsModule } from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input'
 
 
 
@@ -36,7 +39,8 @@ import {HttpClientModule} from '@angular/common/http'
     ProductCrudComponent,
     RedDirective,
     ForDirective,
-    ProductCreateComponent
+    ProductCreateComponent,
+    
   ],
   imports: [
     BrowserModule,
@@ -48,7 +52,10 @@ import {HttpClientModule} from '@angular/common/http'
     MatCardModule,
     MatButtonModule,
     MatSnackBarModule,
-    HttpClientModule, 
+    HttpClientModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule, 
   ],
   providers: [],
   bootstrap: [AppComponent]
